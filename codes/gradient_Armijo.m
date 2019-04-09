bbe008d1cea37167ff29fbbf7dd0b08c0e926732
Astro <m.rahm7n@gmail.com>
2019-04-09 19:17:40 +0200

    Update gradient_Armijo.m

--- codes/gradient_Armijo.m
+++ codes/gradient_Armijo.m
@@ -1,6 +1,7 @@
 %% - Solve the @problem With the
 %  - @Armijo inexact line search
 %---------------------------------------------------------
+clear; close all; clc; 
 
 tolerance = 1e-3;
 % line search setting
@@ -8,29 +9,30 @@ alpha = 0.1;
 gamma = 0.9;
 iter = 0 ;
 tbar=1;
-x0 = [0 0]';
+x0 = [0; 0];
 
 % @INITIALIZE X
 x = x0 ;
 
 % RETURN the function @fun and it's gradient @g
 [fun, g] = f(x);
-fprintf('DEBUG: %i %f\n',iter,norm(g))
-g=[3 1];
-while norm(g) > tolerance    
-    % search direction
-    [fun, g] = f(x);
+
+while norm(g) > tolerance
+    
+    % get the function f(x) @fun and the Gradient @g
     fprintf('DEBUG: %i %f\n',iter,norm(g))
 
     d = -g;
     t = tbar;
-    [g, fun] = f(x);
     % Armijo
     while f(x+t*d) > fun + alpha*g'*d*t
-        t = gamma*t;
+        t = gamma*t ;
     end
+        
     fprintf('DEBUG: %i %f\n',iter,norm(g))
-    x=x+t*d;
+    x = x + t*d ;
+    
+    [fun, g] = f(x);
     iter=iter+1;
-
+    
 end
