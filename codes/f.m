4f1854157b90799afe4f7c420e7f56f7f871fe9a
Astro <m.rahm7n@gmail.com>
2019-04-09 18:47:03 +0200

    the function @f is used within the script gradient_Armijo.m

---
+++ codes/f.m
@@ -0,0 +1,7 @@
+function [fun, g] = f(x)
+    % @function exercise 6
+    fun = 2*x(1)^4+3*x(2)^4+2*x(1)^2+4*x(2)^2+x(1)*x(2)-3*x(1)-2*x(2);
+    % @return the gradient 
+    g = [ 8*x(1)^3+4*x(1)+x(2)-3 12*x(2)^3+8*x(2)+x(1)-2 ];
+end
+
